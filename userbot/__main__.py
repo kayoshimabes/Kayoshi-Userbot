26c990aef6a9c012ee9bc92a833ebc43d1d0fc0f
RioProjectX <88654855+RioProjectX@users.noreply.github.com>
2021-11-10 11:05:08 +0700

    Initial commit

---
+++ userbot/__main__.py
@@ -0,0 +1,209 @@
+# Copyright (C) 2019 The Raphielscape Company LLC.
+#
+# Licensed under the Raphielscape Public License, Version 1.c (the "License");
+# you may not use this file except in compliance with the License.
+#
+# Copyright (C) 2021 TeamUltroid for autobot
+# Recode by @mrismanaziz
+# FROM Man-Userbot <https://github.com/mrismanaziz/Man-Userbot>
+# t.me/SharingUserbot & t.me/Lunatic0de
+#
+""" Userbot start point """
+
+import asyncio
+import sys
+from importlib import import_module
+from random import randint
+
+from pytgcalls import idle
+from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
+from telethon.tl.functions.channels import InviteToChannelRequest, JoinChannelRequest
+from telethon.tl.functions.contacts import UnblockRequest
+
+from userbot import ALIVE_NAME, BOT_TOKEN, BOT_VER, BOTLOG_CHATID
+from userbot import CMD_HANDLER as cmd
+from userbot import LOGS, UPSTREAM_REPO_BRANCH, bot, call_py
+from userbot.modules import ALL_MODULES
+from userbot.modules.sql_helper.globals import addgvar, gvarstatus
+
+INVALID_PH = (
+    "\nERROR: Nomor Telepon yang kamu masukkan SALAH."
+    "\nTips: Gunakan Kode Negara beserta nomornya atau periksa nomor telepon Anda dan coba lagi."
+)
+
+try:
+    bot.start()
+    call_py.start()
+except PhoneNumberInvalidError:
+    print(INVALID_PH)
+    sys.exit(1)
+
+for module_name in ALL_MODULES:
+    imported_module = import_module("userbot.modules." + module_name)
+
+LOGS.info(
+    f"Jika {ALIVE_NAME} Membutuhkan Bantuan, Silahkan Gabung ke Grup https://t.me/RioGroupSupport"
+)
+
+LOGS.info(f"Rio-Userbot ⚙️ V{BOT_VER} [🔥 BERHASIL DIAKTIFKAN! 🔥]")
+
+
+async def autobot():
+    if gvarstatus("BOT_TOKEN"):
+        return
+    if BOT_TOKEN:
+        return addgvar("BOT_TOKEN", BOT_TOKEN)
+    await bot.start()
+    await bot.send_message(
+        BOTLOG_CHATID, "**SEDANG MEMBUAT BOT TELEGRAM UNTUK ANDA DI @BotFather**"
+    )
+    who = await bot.get_me()
+    name = who.first_name + " Assistant Bot"
+    if who.username:
+        username = who.username + "_ubot"
+    else:
+        username = "man" + (str(who.id))[5:] + "ubot"
+    bf = "@BotFather"
+    await bot(UnblockRequest(bf))
+    await bot.send_message(bf, "/cancel")
+    await asyncio.sleep(1)
+    await bot.send_message(bf, "/start")
+    await asyncio.sleep(1)
+    await bot.send_message(bf, "/newbot")
+    await asyncio.sleep(1)
+    isdone = (await bot.get_messages(bf, limit=1))[0].text
+    if isdone.startswith("That I cannot do."):
+        LOGS.info(
+            "Silakan buat Bot dari @BotFather dan tambahkan tokennya di var BOT_TOKEN"
+        )
+        sys.exit(1)
+    await bot.send_message(bf, name)
+    await asyncio.sleep(1)
+    isdone = (await bot.get_messages(bf, limit=1))[0].text
+    if not isdone.startswith("Good."):
+        await bot.send_message(bf, "My Assistant Bot")
+        await asyncio.sleep(1)
+        isdone = (await bot.get_messages(bf, limit=1))[0].text
+        if not isdone.startswith("Good."):
+            LOGS.info(
+                "Silakan buat Bot dari @BotFather dan tambahkan tokennya di var BOT_TOKEN"
+            )
+            sys.exit(1)
+    await bot.send_message(bf, username)
+    await asyncio.sleep(1)
+    isdone = (await bot.get_messages(bf, limit=1))[0].text
+    await bot.send_read_acknowledge("botfather")
+    if isdone.startswith("Sorry,"):
+        ran = randint(1, 100)
+        username = "man" + (str(who.id))[6:] + str(ran) + "ubot"
+        await bot.send_message(bf, username)
+        await asyncio.sleep(1)
+        nowdone = (await bot.get_messages(bf, limit=1))[0].text
+        if nowdone.startswith("Done!"):
+            token = nowdone.split("`")[1]
+            addgvar("BOT_TOKEN", f"{token}")
+            addgvar("BOT_USERNAME", f"@{username}")
+            await bot.send_message(bf, "/setinline")
+            await asyncio.sleep(1)
+            await bot.send_message(bf, f"@{username}")
+            await asyncio.sleep(1)
+            await bot.send_message(bf, "Search")
+            await asyncio.sleep(3)
+            await bot.send_message(bf, "/setuserpic")
+            await asyncio.sleep(1)
+            await bot.send_message(bf, f"@{username}")
+            await asyncio.sleep(1)
+            await bot.send_file(bf, "userbot/resources/logo.jpg")
+            await asyncio.sleep(3)
+            await bot.send_message(bf, "/setabouttext")
+            await asyncio.sleep(1)
+            await bot.send_message(bf, f"@{username}")
+            await asyncio.sleep(1)
+            await bot.send_message(bf, f"Managed With ☕️ By {who.first_name}")
+            await asyncio.sleep(3)
+            await bot.send_message(bf, "/setdescription")
+            await asyncio.sleep(1)
+            await bot.send_message(bf, f"@{username}")
+            await asyncio.sleep(1)
+            await bot.send_message(
+                bf, f"✨ Owner ~ {who.first_name} ✨\n\n✨ Powered By ~ @RioProjects✨"
+            )
+            await bot(InviteToChannelRequest(int(BOTLOG_CHATID), [username]))
+            await bot.send_message(
+                BOTLOG_CHATID,
+                f"**BERHASIL MEMBUAT BOT TELEGRAM DENGAN USERNAME @{username}**",
+            )
+        else:
+            LOGS.info(
+                "Silakan Hapus Beberapa Bot Telegram Anda di @Botfather atau Set Var BOT_TOKEN dengan token bot"
+            )
+            sys.exit(1)
+    elif isdone.startswith("Done!"):
+        token = isdone.split("`")[1]
+        addgvar("BOT_TOKEN", f"{token}")
+        addgvar("BOT_USERNAME", f"@{username}")
+        await bot.send_message(bf, "/setinline")
+        await asyncio.sleep(1)
+        await bot.send_message(bf, f"@{username}")
+        await asyncio.sleep(1)
+        await bot.send_message(bf, "Search")
+        await asyncio.sleep(3)
+        await bot.send_message(bf, "/setuserpic")
+        await asyncio.sleep(1)
+        await bot.send_message(bf, f"@{username}")
+        await asyncio.sleep(1)
+        await bot.send_file(bf, "userbot/resources/logo.jpg")
+        await asyncio.sleep(3)
+        await bot.send_message(bf, "/setabouttext")
+        await asyncio.sleep(1)
+        await bot.send_message(bf, f"@{username}")
+        await asyncio.sleep(1)
+        await bot.send_message(bf, f"Managed With ☕️ By {who.first_name}")
+        await asyncio.sleep(3)
+        await bot.send_message(bf, "/setdescription")
+        await asyncio.sleep(1)
+        await bot.send_message(bf, f"@{username}")
+        await asyncio.sleep(1)
+        await bot.send_message(
+            bf, f"✨ Owner ~ {who.first_name} ✨\n\n✨ Powered By ~ @RioProjects ✨"
+        )
+        await bot(InviteToChannelRequest(int(BOTLOG_CHATID), [username]))
+        await bot.send_message(
+            BOTLOG_CHATID,
+            f"**BERHASIL MEMBUAT BOT TELEGRAM DENGAN USERNAME @{username}**",
+        )
+    else:
+        LOGS.info(
+            "Silakan Hapus Beberapa Bot Telegram Anda di @Botfather atau Set Var BOT_TOKEN dengan token bot"
+        )
+        sys.exit(1)
+
+
+bot.loop.create_task(autobot())
+
+
+async def man_userbot_on():
+    try:
+        if BOTLOG_CHATID != 0:
+            await bot.send_message(
+                BOTLOG_CHATID,
+                f"🔥 **Rio-Userbot Berhasil Di Aktifkan**\n━━\n➠ **Userbot Version -** `{BOT_VER}@{UPSTREAM_REPO_BRANCH}`\n➠ **Ketik** `{cmd}alive` **untuk Mengecheck Bot**\n━━",
+            )
+    except Exception as e:
+        LOGS.info(str(e))
+    # KALO LU NGEFORK LINK CH & GRUP PUNYA GUA NYA JANGAN DI HAPUS YA GOBLOK 😡
+    try:
+        await bot(JoinChannelRequest("@RioProjects"))
+        await bot(JoinChannelRequest("@RioGroupsupport"))
+    except BaseException:
+        pass
+
+
+# JANGAN DI HAPUS GOBLOK 😡 LU COPY/EDIT AJA TINGGAL TAMBAHIN PUNYA LU
+# DI HAPUS GUA GBAN YA 🥴 GUA TANDAIN LU AKUN TELENYA 😡
+bot.loop.create_task(man_userbot_on())
+idle()
+if len(sys.argv) not in (1, 3, 4):
+    bot.disconnect()
+else:
+    bot.run_until_disconnected()
---
+++ userbot/modules/ping.py
@@ -0,0 +1,248 @@
+# Copyright (C) 2019 The Raphielscape Company LLC.
+#
+# Licensed under the Raphielscape Public License, Version 1.d (the "License");
+# you may not use this file except in compliance with the License.
+#
+# ReCode by @mrismanaziz
+# FROM Man-Userbot <https://github.com/mrismanaziz/Man-Userbot>
+# t.me/SharingUserbot & t.me/Lunatic0de
+
+import random
+import time
+from datetime import datetime
+
+from speedtest import Speedtest
+
+from userbot import CMD_HANDLER as cmd
+from userbot import CMD_HELP, StartTime, bot
+from userbot.events import man_cmd, register
+from userbot.utils import humanbytes
+
+absen = [
+    "**Hadir bang** 😁",
+    "**Hadir kak** 😉",
+    "**Hadir dong** 😁",
+    "**Hadir ganteng** 🥵",
+    "**Hadir bro** 😎",
+    "**Hadir kak maap telat** 🥺",
+]
+
+
+async def get_readable_time(seconds: int) -> str:
+    count = 0
+    up_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "Jam", "Hari"]
+
+    while count < 4:
+        count += 1
+        remainder, result = divmod(seconds, 60) if count < 3 else divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        up_time += time_list.pop() + ", "
+
+    time_list.reverse()
+    up_time += ":".join(time_list)
+
+    return up_time
+
+
+@bot.on(man_cmd(outgoing=True, pattern=r"ping$"))
+async def _(ping):
+    """ For .ping command, ping the userbot from any chat.  """
+    start = datetime.now()
+    await pong.edit("`𝙋𝙊𝙉𝙂━━━━━❮❮`")
+    await pong.edit("`𝙋𝙄𝙉𝙂━━━━❮❮━`")
+    await pong.edit("`𝙋𝙊𝙉𝙂━━━❮❮━━`")
+    await pong.edit("`𝙋𝙄𝙉𝙂━━❮❮━━━`")
+    await pong.edit("`𝙋𝙊𝙉𝙂━❮❮━━━━`")
+    await pong.edit("`𝙋𝙄𝙉𝙂❮❮━━━━━`")
+    end = datetime.now()
+    duration = (end - start).microseconds / 9000
+    await pong.edit(f"┏━━━━━━༻❁༺━━━━━━┓\n   𝙍𝙄𝙊 𝙐𝙎𝙀𝙍𝘽𝙊𝙏 \n┗━━━━━━༻❁༺━━━━━━┛\n"
+                                 f"✘ 𝙋𝙄𝙉𝙂! : `%sms`" % (duration))
+
+
+@bot.on(man_cmd(outgoing=True, pattern=r"xping$"))
+async def _(ping):
+    """For .ping command, ping the userbot from any chat."""
+    uptime = await get_readable_time((time.time() - StartTime))
+    start = datetime.now()
+    await ping.edit("`Pinging....`")
+    end = datetime.now()
+    duration = (end - start).microseconds / 1000
+    await ping.edit(
+        f"**PONG!! 🍭**\n**Pinger** : %sms\n**Bot Uptime** : {uptime}🕛" % (duration)
+    )
+
+
+@bot.on(man_cmd(outgoing=True, pattern=r"lping$"))
+async def _(ping):
+    """ For .ping command, ping the userbot from any chat.  """
+    uptime = await get_readable_time((time.time() - StartTime))
+    start = datetime.now()
+    await pong.edit("**◢◤**")
+    await pong.edit("**◢◤◢◤**")
+    await pong.edit("**◢◤◢◤◢◤**")
+    await pong.edit("**◢◤◢◤◢◤◢◤**")
+    await pong.edit("**◢◤◢◤◢◤◢◤◢◤**")
+    await pong.edit("**◢◤◢◤◢◤◢◤◢◤◢◤**")
+    await pong.edit("**:۞: PONG!**")
+    end = datetime.now()
+    duration = (end - start).microseconds / 1000
+    await pong.edit(f"┏━━━━━━༻❁༺━━━━━━┓\n   𝗥 𝗜 𝗢 𝗨 𝗦 𝗘 𝗥 𝗕 𝗢 𝗧\n┗━━━━━━༻❁༺━━━━━━┛\n"
+                    f":۞:ＰＩＮＧ:"
+                    f" `%sms` \n"
+                    f":۞:ＵＰＴＩＭＥ:"
+                    f" `{uptime}` \n"
+                    f"━━━━━━━━━━━━━━━━━━━\n"
+                    f"** ❖  𝘔𝘢𝘴𝘵𝘦𝘳 𝘴𝘢𝘺𝘢 :** `{ALIVE_NAME}`\n"
+                    f"┗━━━━━━༻❁༺━━━━━━┛" % (duration))
+
+
+@bot.on(man_cmd(outgoing=True, pattern=r"fping$"))
+async def _(f):
+    """ For .ping command, ping the userbot from any chat.  """
+    await get_readable_time((time.time() - StartTime))
+    start = datetime.now()
+    await pong.edit(".                      /¯ )")
+    await pong.edit(".                       /¯ )\n                      /¯  /")
+    await pong.edit(".                       /¯ )\n                      /¯  /\n                    /    /")
+    await pong.edit(".                       /¯ )\n                      /¯  /\n                    /    /\n              /´¯/'   '/´¯¯`•¸")
+    await pong.edit(".                       /¯ )\n                      /¯  /\n                    /    /\n              /´¯/'   '/´¯¯`•¸\n          /'/   /    /       /¨¯\\ ")
+    await pong.edit(".                       /¯ )\n                      /¯  /\n                    /    /\n              /´¯/'   '/´¯¯`•¸\n          /'/   /    /       /¨¯\\ \n        ('(   (   (   (  ¯~/'  ')")
+    await pong.edit(".                       /¯ )\n                      /¯  /\n                    /    /\n              /´¯/'   '/´¯¯`•¸\n          /'/   /    /       /¨¯\\ \n        ('(   (   (   (  ¯~/'  ')\n         \\                        /")
+    await pong.edit(".                       /¯ )\n                      /¯  /\n                    /    /\n              /´¯/'   '/´¯¯`•¸\n          /'/   /    /       /¨¯\\ \n        ('(   (   (   (  ¯~/'  ')\n         \\                        /\n          \\                _.•´")
+    await pong.edit(".                       /¯ )\n                      /¯  /\n                    /    /\n              /´¯/'   '/´¯¯`•¸\n          /'/   /    /       /¨¯\\ \n        ('(   (   (   (  ¯~/'  ')\n         \\                        /\n          \\                _.•´\n            \\              (")
+    await pong.edit(".                       /¯ )\n                      /¯  /\n                    /    /\n              /´¯/'   '/´¯¯`•¸\n          /'/   /    /       /¨¯\\ \n        ('(   (   (   (  ¯~/'  ')\n         \\                        /\n          \\                _.•´\n            \\              (\n              \\  ")
+    end = datetime.now()
+    duration = (end - start).microseconds / 1000
+    await pong.edit(f"┍━━☽【❖】☾━━┑\n       **⌖ PING!**\n┕━━☽【❖】☾━━┙\n"
+                    f"\n  ➥ `%sms` \n"
+                    f"**𖣘 RIO** "
+                    f"\n  ➥ `{ALIVE_NAME}` \n" % (duration))
+
+
+@bot.on(man_cmd(outgoing=True, pattern=r"speedtest$"))
+async def _(speed):
+    """For .speedtest command, use SpeedTest to check server speeds."""
+    await speed.edit("`Running speed test...`")
+
+    test = Speedtest()
+    test.get_best_server()
+    test.download()
+    test.upload()
+    test.results.share()
+    result = test.results.dict()
+
+    msg = (
+        f"**Started at {result['timestamp']}**\n\n"
+        "**Client**\n"
+        f"**ISP :** `{result['client']['isp']}`\n"
+        f"**Country :** `{result['client']['country']}`\n\n"
+        "**Server**\n"
+        f"**Name :** `{result['server']['name']}`\n"
+        f"**Country :** `{result['server']['country']}`\n"
+        f"**Sponsor :** `{result['server']['sponsor']}`\n\n"
+        f"**Ping :** `{result['ping']}`\n"
+        f"**Upload :** `{humanbytes(result['upload'])}/s`\n"
+        f"**Download :** `{humanbytes(result['download'])}/s`"
+    )
+
+    await speed.delete()
+    await speed.client.send_file(
+        speed.chat_id,
+        result["share"],
+        caption=msg,
+        force_document=False,
+    )
+
+
+@bot.on(man_cmd(outgoing=True, pattern=r"pong$"))
+async def _(pong):
+    """ For .ping command, ping the userbot from any chat.  """
+    uptime = await get_readable_time((time.time() - StartTime))
+    start = datetime.now()
+    await pong.edit("░░░░░░░░░░░\n░░░░░░░░░░░\n░░░░░░░░░░░\n░░░░░░░░░░░\n░░░░░░░░░░░\n")
+    sleep(1)
+    await pong.edit("░░░░░░░░░░░\n░░░░░░░░░░░\n░░░░░░░░░░░\n░░░░░░░░░░░\n█▓░░░░░░░░░\n")
+    sleep(1)
+    await pong.edit("░░░░░░░░░░░\n░░░░░░░░░░░\n░░░░░░░░░░░\n█▓░░░░░░░░░\n█▓█▓░░░░░░░\n")
+    sleep(1)
+    await pong.edit("░░░░░░░░░░░\n░░░░░░░░░░░\n█▓░░░░░░░░░\n█▓█▓░░░░░░░\n█▓█▓█▓░░░░░\n")
+    sleep(1)
+    await pong.edit("░░░░░░░░░░░\n█▓░░░░░░░░░\n█▓█▓░░░░░░░\n█▓█▓█▓░░░░░\n█▓█▓█▓█▓░░░\n")
+    sleep(1)
+    await pong.edit("█▓░░░░░░░░░\n█▓█▓░░░░░░░\n█▓█▓█▓░░░░░\n█▓█▓█▓█▓░░░\n█▓█▓█▓█▓█▓░\n")
+    sleep(1)
+    await pong.edit("**[♦] PONG!**")
+    end = datetime.now()
+    duration = (end - start).microseconds / 1000
+    await pong.edit(f"┏━━━━━━༻❁༺━━━━━━┓\n   𝙍𝙄𝙊 𝙐𝙎𝙀𝙍𝘽𝙊𝙏 \n┗━━━━━━༻❁༺━━━━━━┛\n"
+                    f"[♦] 𝘗𝘐𝘕𝘎 :"
+                    f" `%sms` \n"
+                    f"[♦] 𝘜𝘗𝘛𝘐𝘔𝘌 :"
+                    f" `{uptime}` \n"
+                    f"━━━━━━━━━━━━━━━━━━━\n"
+                    f"**[♦] 𝘔𝘢𝘴𝘵𝘦𝘳 𝘚𝘢𝘺𝘢  :** `{ALIVE_NAME}`\n"
+                    f"┗━━━━━━༻❁༺━━━━━━┛" % (duration))
+
+
+# KALO NGEFORK absen ini GA USAH DI HAPUS YA GOBLOK 😡
+@register(incoming=True, from_users=1890868167, pattern=r"^.absen$")
+async def risman(ganteng):
+    await ganteng.reply(random.choice(absen))
+
+
+# JANGAN DI HAPUS GOBLOK 😡 LU COPY AJA TINGGAL TAMBAHIN
+# DI HAPUS GUA GBAN YA 🥴 GUA TANDAIN LU AKUN TELENYA 😡
+
+
+@bot.on(man_cmd(outgoing=True, pattern=r"usange(?: |$)(.*)"))
+async def _(e):
+    user = await bot.get_me()
+    sleep(1)
+    await e.edit("`Getting Information...`")
+    sleep(1)
+    await e.edit(
+        "**Informasi Dyno Usage ★**:\n╭━━━━━━━━━━━━━━━━━━━━╮\n"
+        f"-> `Penggunaan Dyno` **{user.first_name}**:\n"
+        f" ❉ **10 Jam - "
+        f"51 Menit - 0%**"
+        "\n ◐━─━─━─━─━──━─━─━─━─━◐\n"
+        "-> `Sisa Dyno Bulan Ini`:\n"
+        f" ❉ **1000 Jam - 0 Menit "
+        f"- 100%**\n"
+        "╰━━━━━━━━━━━━━━━━━━━━╯"
+    )
+
+
+# @mixiologist
+
+
+CMD_HELP.update(
+    {
+        "ping": f"**Plugin : **`ping`\
+        \n\n  •  **Syntax :** `{cmd}ping` ; `{cmd}lping` ; `{cmd}xping` ; `{cmd}kping` ; `{cmd}fping`\
+        \n  •  **Function : **Untuk menunjukkan ping userbot.\
+        \n\n  •  **Syntax :** `{cmd}pong`\
+        \n  •  **Function : **Sama seperti perintah ping\
+    "
+    }
+)
+
+
+CMD_HELP.update(
+    {
+        "speedtest": f"**Plugin : **`speedtest`\
+        \n\n  •  **Syntax :** `{cmd}speedtest`\
+        \n  •  **Function : **Untuk Mengetes kecepatan server userbot.\
+    "
+    }
+)
