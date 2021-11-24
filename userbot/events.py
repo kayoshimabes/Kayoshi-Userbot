34eb6e89b22fb26a1dd74d07de27a7236f7062ec
Kayoshirobot <91388336+kayoshimabes@users.noreply.github.com>
2021-11-24 22:35:16 +0700

    Update events.py

--- userbot/events.py
+++ userbot/events.py
@@ -182,8 +182,8 @@ def register(**args):
                 if not disable_errors:
                     date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
-                    text = "**✘ RIO-USERBOT ERROR REPORT ✘**\n\n"
-                    link = "[Group Support](https://t.me/riogroupsupport)"
+                    text = "**✘ KAYOSHI-USERBOT ERROR REPORT ✘**\n\n"
+                    link = "[Group Support](https://t.me/TurboGroupSupport)"
                     text += "Jika mau, Anda bisa melaporkan error ini, "
                     text += f"Cukup forward saja pesan ini ke {link}.\n\n"
 
@@ -192,7 +192,7 @@ def register(**args):
                     ftext += "\nkami hanya mencatat fakta error dan tanggal,"
                     ftext += "\nkami menghormati privasi Anda."
                     ftext += "\nJika mau, Anda bisa melaporkan error ini,"
-                    ftext += "\ncukup forward saja pesan ini ke @riogroupsupport"
+                    ftext += "\ncukup forward saja pesan ini ke @TurboGroupSupport"
                     ftext += "\n================================\n\n"
                     ftext += "--------BEGIN USERBOT TRACEBACK LOG--------\n"
                     ftext += "\nTanggal : " + date
